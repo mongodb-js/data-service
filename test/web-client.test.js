20f6602e55f68595040d189f5ef39036225790ca
Durran Jordan <durran@gmail.com>
2017-07-31 14:28:36 -0400

    Skip WebClient tests for now

--- test/web-client.test.js
+++ test/web-client.test.js
@@ -4,13 +4,16 @@ var expect = helper.expect;
 
 var WebClient = require('../lib/web-client');
 
+/**
+ * @note: Durran: Skipped for now until we find a better way to automate this on Travis.
+ */
 describe('WebClient', function() {
   this.slow(10000);
   this.timeout(20000);
   let userId;
   var client = new WebClient(helper.stitchConnection);
 
-  describe('#connect', function() {
+  describe.skip('#connect', function() {
     it('yields a Stitch Client', function(done) {
       client.connect(function(error, stitchClient) {
         expect(error).to.equal(null);
@@ -21,7 +24,7 @@ describe('WebClient', function() {
     });
   });
 
-  describe('#insertOne', function() {
+  describe.skip('#insertOne', function() {
     after(function(done) {
       client.deleteOne('data-service.test', { owner_id: userId }, {}, function() {
         done();
@@ -37,7 +40,7 @@ describe('WebClient', function() {
     });
   });
 
-  describe('#count', function() {
+  describe.skip('#count', function() {
     before(function(done) {
       client.insertOne('data-service.test', { owner_id: userId, a: 1 }, {}, function() {
         done();
@@ -59,7 +62,7 @@ describe('WebClient', function() {
     });
   });
 
-  describe('#find', function() {
+  describe.skip('#find', function() {
     before(function(done) {
       client.insertOne('data-service.test', { owner_id: userId, a: 1 }, {}, function() {
         done();
@@ -81,7 +84,7 @@ describe('WebClient', function() {
     });
   });
 
-  describe('#aggregate', function() {
+  describe.skip('#aggregate', function() {
     before(function(done) {
       client.insertOne('data-service.test', { owner_id: userId, a: 1 }, {}, function() {
         done();
